4016e9dc49d039fd320ca2d84f2ce322c4fde85f
Gustavo <gustavoh.santos735@gmail.com>
2022-12-13 21:43:21 -0300

    prisma config

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,11 +1,20 @@
 import express from "express"
-
+import { PrismaClient } from "@prisma/client"
+ 
 const app = express()
+const prisma = new PrismaClient({
+    log: ['error']
+})
 
-app.get('/', (request, response)=>{
-    return response.status(200).json({
-        name: 'teste'
-    })
+app.get('/users', async (request, response) => {
+
+    try {
+        const users = await prisma.user.findMany()
+        return response.status(200).json(users)
+    } catch (error) {
+        return response.status(200).json(error)
+    }
+    
 })
 
 app.listen(3000, ()=>{console.log("Server running at http://localhost:3000");
