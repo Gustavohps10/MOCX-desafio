fa535d0987bcee5edc8b663d9473a6877ce88ade
Gustavo <gustavoh.santos735@gmail.com>
2022-12-14 00:34:14 -0300

    add - cpf validation

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,5 +1,6 @@
 import express, { json } from "express"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, Prisma } from "@prisma/client"
+import { cpf as cpfValidator} from "cpf-cnpj-validator"
  
 const app = express()
 app.use(json())
@@ -20,37 +21,62 @@ app.get('/users', async (request, response) => {
 
 app.post('/users', async (request, response) => {
     const body = request.body
+    const name = body.name.trim()
+    const cpf = body.cpf.trim()
+    const birthDate = body.birthDate.trim()
 
     try {
+        if(!name || !cpf || !birthDate){
+            throw new Error("Preencha todos os campos");
+        }
+
+        if(!cpfValidator.isValid(cpf)){
+            throw new Error("CPF inválido");   
+        }
+
         const user = await prisma.user.create({
             data: {
-                name: body.name,
-                cpf: body.cpf,
-                birthDate: body.birthDate
+                name: name,
+                cpf: cpf.replace(/\D/g,''),
+                birthDate: birthDate
             }
         })
 
         return response.status(201).json(user)
-    } catch (error) {
+    } catch (error: any) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2002') {
+              return response.status(409).json({
+                error: 'Já existe um usuário com esse CPF'
+              })
+            }
+        }
+
         return response.status(500).json({
-            error: "Ocorreu um erro, tente novamente"
+            error: error.message
         })
     }
     
 })
 
-app.put('/users/:id',async (request, response) => {
+app.put('/users/:id', async (request, response) => {
     const id = request.params.id
     const body = request.body
+    const name = body.name.trim()
+    const birthDate = body.birthDate.trim()
 
     try {
+        if(!name || !birthDate){
+            throw new Error("Preencha todos os campos");
+        }
+
         const updatedUser = await prisma.user.update({
             where: {
               id: id,
             },
             data: {
-              name: body.name,
-              birthDate: body.birthDate
+              name: name,
+              birthDate: birthDate
             },
         })
         return response.status(200).json(updatedUser)
